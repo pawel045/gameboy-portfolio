6cdf98b4f78f4829e4d6fc64ee6c5b42e79efa75
Paweł <proszczyk96@gmail.com>
2025-05-15 18:56:02 +0200

    prepare for deploy 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace with your GitHub repo name
 export default defineConfig({
   plugins: [react()],
+  base: '/gameboy-portfolio/', // IMPORTANT: Must include slashes
 })
